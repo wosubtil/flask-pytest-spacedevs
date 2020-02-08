277fa2b3845fcff4b2f84cd1ced6c1df4af7c9f6
Welinton <welinton@realizati.com.br>
2020-02-08 14:55:57 -0300

    Fixed formatting

--- config.py
+++ config.py
@@ -2,17 +2,22 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+
 class Development(Config):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'dev.sqlite')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
+        os.path.join(basedir, 'dev.sqlite')
+
 
 class Testing(Config):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'testing.sqlite')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
+        os.path.join(basedir, 'testing.sqlite')
+
 
 config = dict(
-    development = Development(),
-    testing = Testing()
+    development=Development(),
+    testing=Testing()
 )
-
--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from app.models import User
 
 app = create_app('development')
 
+
 @app.shell_context_processor
 def make_context_processor():
     return dict(
-       app=app,
-       db=db,
-       User=User
+        app=app,
+        db=db,
+        User=User
     )
+
 
 if __name__ == '__main__':
     app.run(debug=True)
